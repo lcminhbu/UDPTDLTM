2649519a04ade7cb9688856e028ecdd39af0dea1
dolekhanhdang <71361533+dolekhanhdang@users.noreply.github.com>
2022-10-03 07:44:27 +0700

    Get menu
    Add get menu function

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import csv
 from threading import Thread
-import random
+from random import randint
 import urllib
 import pandas as pd
 import logging
@@ -120,7 +121,8 @@ def handle_restaurant_page(link_list):
             metadata.append(review_count[0].text)
             metadata.append(price_range)
             metadata.extend([open_hours[0], open_hours[-1]])
-            need_to_remove = ["Thích hợp", "Giờ nhận khách cuối", "Thời gian chuẩn bị", "Nghỉ lễ", "Thể loại", "Sức chứa",
+            need_to_remove = ["Thích hợp", "Giờ nhận khách cuối", "Thời gian chuẩn bị", "Nghỉ lễ", "Thể loại",
+                              "Sức chứa",
                               "Phong cách ẩm thực", "Phù hợp với", "Phục vụ các món"]
             infor_check = {}
             for index in need_to_remove:
@@ -128,17 +130,51 @@ def handle_restaurant_page(link_list):
             for index in infor_area:
                 for title in need_to_remove:
                     if title in index.text:
-                        infor_check[title] = index.text.replace(title, "").replace('\n', "").replace('\xa0','').strip()
+                        infor_check[title] = index.text.replace(title, "").replace('\n', "").replace('\xa0', '').strip()
                         break
             for index in need_to_remove:
                 metadata.append(infor_check[index])
             available = soup.findAll(class_="microsite-box-content")[-1]
-            available = [index for index in available.text.split('\n') if index !=""]
+            available = [index for index in available.text.split('\n') if index != ""]
             metadata.append(available)
             metadata_list.append(metadata)
         except:
             print("An exception occurred")
     return metadata_list
+
+
+def handle_menu(link_list):
+    menu_list = []
+    for index in range(len(link_list)):
+        print(f"{index}-{len(link_list)}")
+        menu = []
+        try:
+            driver = webdriver.Edge()
+            driver.get("https://www.shopeefood.vn" + link_list[index])
+            time.sleep(2)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            check_available = soup.find(class_="txt-bold font15")
+            if check_available and check_available.text == "Rất tiếc, bài viết không tồn tại !":
+                driver.close()
+            else:
+                body = driver.find_element(by=By.CSS_SELECTOR, value="body")
+                for dump in range(50):
+                    if dump != 0:
+                        body.send_keys(Keys.PAGE_DOWN)
+                        time.sleep(0.5)
+                    soup = BeautifulSoup(driver.page_source, 'html.parser')
+                    data_sample = soup.findAll(class_="item-restaurant-row")
+                    menu.extend(data_sample)
+                driver.close()
+                menu = pd.unique(menu).tolist()
+                for disk in menu:
+                    menu_list.append(
+                        [link_list[index], disk.find_next(text=True).strip(), disk.contents[0].contents[2].text])
+                time.sleep(randint(1, 4))
+        except:
+            print("An exception occurred")
+
+    return menu_list
 
 
 def get_all_page_link_main():
@@ -180,12 +216,35 @@ def get_page_information(csv_file):
                                       "serve_score", "price_score", "address", "district", "views", "review_count",
                                       "price_range", "open hours", "close hours", "suitable_time", "last_pickup_time",
                                       "prepare_time", "holiday", "type", "capacity", "style", "suitable_with",
-                                      "serves","available"])
+                                      "serves", "available"])
     dataframe.to_csv('Restaurant_information.csv', sep='\t', encoding='utf-8')
 
 
+def get_menu_information(csv_file):
+    menu = []
+    data = pd.read_csv(csv_file, sep='\t', encoding='utf-8')
+    links = data['Link'].tolist()[1900:2200]  # -2182
+    print(len(links))
+    n = 18
+    data_divide = [links[i:i + n] for i in range(0, len(links), n)]
+    print(len(data_divide))
+    my_threads = []
+    for index in range(len(data_divide)):
+        thread = ThreadWithReturnValue(target=handle_menu, args=([data_divide[index]]))
+        thread.start()
+        print(index)
+        my_threads.append(thread)
+    for index in my_threads:
+        menu.extend(index.join())
+    dataframe = pd.DataFrame(menu,
+                             columns=["Link", "Food_name", "Price"])
+    dataframe.to_csv('Menu_31.csv', sep='\t', encoding='utf-8')
+
+
 # if __name__ == "__main__":
-#test = handle_restaurant_page(['ho-chi-minh/morico-modern-japanese-restaurant-cafe-le-loi','ho-chi-minh/tuktuk-thai-bistro-hoa-mai'])
-data = pd.read_csv('Restaurant_information.csv', sep='\t', encoding='utf-8')
-#get_page_information('Restaurant_list.csv')
+# test = handle_restaurant_page(['ho-chi-minh/morico-modern-japanese-restaurant-cafe-le-loi','ho-chi-minh/tuktuk-thai-bistro-hoa-mai'])
+# data = pd.read_csv('Restaurant_information.csv', sep='\t', encoding='utf-8')
+# get_page_information('Restaurant_list.csv')
+# data = handle_menu(["ho-chi-minh/i-love-kem-phan-huy-ich"])
+get_menu_information('Restaurant_list.csv')
 print(1)

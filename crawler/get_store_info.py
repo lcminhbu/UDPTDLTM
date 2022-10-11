287bd1bd85a3aa725c317880250f064df378c075
dolekhanhdang <71361533+dolekhanhdang@users.noreply.github.com>
2022-10-11 16:51:15 +0700

    Fix seleniuim find_element

--- crawler/get_store_info.py
+++ crawler/get_store_info.py
@@ -38,7 +38,8 @@ score_dict = {
 
 def get_parking_lots(driver: webdriver.Chrome):
     checker = driver.find_element("xpath",
-        '''//span[@ng-bind="data.NearbyParkingPlaces.Items.length + '/' + data.NearbyParkingPlaces.Total"]''').text
+                                  '''//span[@ng-bind="data.NearbyParkingPlaces.Items.length + '/' + 
+                                  data.NearbyParkingPlaces.Total"]''').text
     [cur, max] = checker.split('/')
     cur = int(cur)
     max = int(max)
@@ -47,20 +48,21 @@ def get_parking_lots(driver: webdriver.Chrome):
     while cur < max:
         WebDriverWait(driver, 2).until(
             EC.element_to_be_clickable((By.ID, "continueShowReview")))
-        see_more_btn = driver.find_element_by_id("continueShowReview")
+        see_more_btn = driver.find_element(By.ID, "continueShowReview")
         see_more_btn.click()
         checker = driver.find_element("xpath",
-            '''//span[@ng-bind="data.NearbyParkingPlaces.Items.length + '/' + data.NearbyParkingPlaces.Total"]''').text
+                                      '''//span[@ng-bind="data.NearbyParkingPlaces.Items.length + '/' + 
+                                      data.NearbyParkingPlaces.Total"]''').text
         cur = int(checker.split('/')[0])
     # parking_lots = driver.find_elements_by_css_selector("#res-nearby-content > div.ldc-items-list.ldc-items-row > div > ul > li")
-    parking_lots = driver.find_elements_by_class_name("ldc-item-header")
+    parking_lots = driver.find_elements(By.CLASS_NAME, "ldc-item-header")
     if len(parking_lots) != max:
         log.error("Parking lot count wrong")
         return [], -1
     pls = []
     for pl in parking_lots:
-        pl_name = pl.find_element_by_class_name("ldc-item-h-name").text
-        address = pl.find_elements_by_class_name("ldc-item-h-address")
+        pl_name = pl.find_element(By.CLASS_NAME, "ldc-item-h-name").text
+        address = pl.find_elements(By.CLASS_NAME, "ldc-item-h-address")
         pl_position = address[0].text
         pl_opening_time = address[1].text
         pl_capacity = address[2].text
@@ -78,14 +80,14 @@ def get_info(link, driver: webdriver.Chrome):
         driver.get(link)
         driver.implicitly_wait(15)
 
-        name = driver.find_element("xpath",'//h1[@itemprop="name"]').text
-        review_count = driver.find_element_by_class_name("microsite-review-count").text
-        addr = driver.find_element("xpath",'//span[@itemprop="streetAddress"]').text
-        district = driver.find_element("xpath",'//span[@itemprop="addressLocality"]').text
+        name = driver.find_element("xpath", '//h1[@itemprop="name"]').text
+        review_count = driver.find_element(By.CLASS_NAME, "microsite-review-count").text
+        addr = driver.find_element("xpath", '//span[@itemprop="streetAddress"]').text
+        district = driver.find_element("xpath", '//span[@itemprop="addressLocality"]').text
 
-        avg_score = driver.find_element_by_class_name("microsite-point-avg ").text
-        points = driver.find_elements_by_css_selector("div.microsite-top-points")
-        views = driver.find_element_by_class_name("total-views").find_element_by_tag_name("span").text
+        avg_score = driver.find_element(By.CLASS_NAME, "microsite-point-avg ").text
+        points = driver.find_elements(By.CSS_SELECTOR, "div.microsite-top-points")
+        views = driver.find_element(By.CLASS_NAME, "total-views").find_element(By.TAG_NAME, "span").text
 
         t = {
             "name": name,
@@ -98,12 +100,12 @@ def get_info(link, driver: webdriver.Chrome):
         for p in points:
             try:
                 try:
-                    score = p.find_element_by_tag_name("span").text
+                    score = p.find_element(By.TAG_NAME, "span").text
                 except:
                     score = -1
                     log.warning("Score not found")
                 finally:
-                    label = p.find_element_by_class_name("label").text
+                    label = p.find_element(By.CLASS_NAME, "label").text
                     if label not in score_dict:
                         log.error(label + "not in dictionary")
                     else:
@@ -111,40 +113,40 @@ def get_info(link, driver: webdriver.Chrome):
             except:
                 log.warning("Score: list index out of range")
         try:
-            driver.find_element_by_class_name("view-all-menu").click()
-            food_qr_code = driver.find_element_by_class_name("food-qrcode-footer-btn")
+            driver.find_element(By.CLASS_NAME, "view-all-menu").click()
+            food_qr_code = driver.find_element(By.CLASS_NAME, "food-qrcode-footer-btn")
             food_link = food_qr_code.get_attribute("href")
-            driver.find_element_by_class_name("modalCloseImg.simplemodal-close").click()  # Close pop-up
+            driver.find_element(By.CLASS_NAME, "modalCloseImg.simplemodal-close").click()  # Close pop-up
         except:
             log.info("Food link empty")
             food_link = ""
         t['food_link'] = food_link
 
-        other_info = driver.find_elements_by_class_name("new-detail-info-area")
+        other_info = driver.find_elements(By.CLASS_NAME, "new-detail-info-area")
         for i in other_info:
-            label = dictionary[i.find_element_by_css_selector("div:nth-child(1)").text]
-            value = i.find_element_by_css_selector("div:nth-child(2)").text
+            label = dictionary[i.find_element(By.CSS_SELECTOR,"div:nth-child(1)").text]
+            value = i.find_element(By.CSS_SELECTOR,"div:nth-child(2)").text
             t[label] = value
 
-        micro_property = driver.find_elements_by_css_selector("div.microsite-res-info-properties > div > div > ul > li")
+        micro_property = driver.find_elements(By.CSS_SELECTOR,"div.microsite-res-info-properties > div > div > ul > li")
         t['available'] = []
         for p in micro_property:
             if p.get_attribute("class") != "none":
-                t['available'].append(p.find_element_by_css_selector("a:nth-child(2)").text)
+                t['available'].append(p.find_element(By.CSS_SELECTOR,"a:nth-child(2)").text)
 
         # log.info("Getting branches")
         # try:
         #     list_tools = driver.find_element_by_css_selector("ul.list-tool")
         #     branch_link = list_tools.find_element_by_link_text("Chi nhánh")
-            # branch_link.click()
-            # branch_list = driver.find_element_by_css_selector("ul.ldc-items-list.ldc-column")
-            # driver.back()
-            # driver.implicitly_wait(15)
+        # branch_link.click()
+        # branch_list = driver.find_element_by_css_selector("ul.ldc-items-list.ldc-column")
+        # driver.back()
+        # driver.implicitly_wait(15)
         # except:
         #     log.info("No branch")
         try:
-            list_tools = driver.find_element_by_css_selector("ul.list-tool")
-            parking_lot_link = list_tools.find_element_by_link_text("Bãi đỗ xe")
+            list_tools = driver.find_element(By.CSS_SELECTOR,"ul.list-tool")
+            parking_lot_link = list_tools.find_element(By.LINK_TEXT,"Bãi đỗ xe")
             parking_lot_link.click()
             t['parking_lots'], t['parking_lots_ammount'] = get_parking_lots(driver)
         except:
